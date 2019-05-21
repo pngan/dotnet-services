241569e37558e8ce3dbca52c1b126570362dbeaa
Phillip Ngan <phillip.ngan@enghouse.com>
2019-05-21 12:49:36 +1200

    Initial commit of hello world example demonstrating
    - dotnet core 3
    - gRPC service
    
    Read README.md for details on how to build and run

---
+++ hello-controller/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Greet;
+using Grpc.Core;
+
+namespace hello_controller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            // The port number here must match the port of the gRPC server
+            var channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(
+                                          new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            await channel.ShutdownAsync();
+            return new string[] { reply.Message };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
